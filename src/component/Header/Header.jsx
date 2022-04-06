39937b1e4eec39b046fcb1f28e9234e9034744b3
Michael Akinsanya <tripleoniks9@gmail.com>
2022-04-06 15:33:36 +0100

    Update Header.jsx

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,6 +1,5 @@
 import "./Header.scss";
 import Logo from "../../component/Logo/Logo";
-import CustomButton from "../CustomButton/CustomButton";
 import { Link } from "react-router-dom";
 
 const Header = () => {
